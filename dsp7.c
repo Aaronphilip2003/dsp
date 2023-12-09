6623e915c988eb722275adac6024aaf0c00ed1b5
Aaron <69317200+Aaronphilip2003@users.noreply.github.com>
2023-12-09 09:11:24 +0530

    Update dsp7.c

--- dsp7.c
+++ dsp7.c
@@ -1,58 +1,75 @@
+
 #include <stdio.h>
-int main()
-{
-int x[15] = {0};
-int h[15] = {0};
-int y[15] = {0};
-int N,L,K,i,j = 0;
-printf("Enter the length of h(n): ");
-scanf("%d",&N);
-printf("Enter the length of x(n): ");
 
-scanf("%d",&L);
-K=N+L-1;
-printf("Enter the values of h(n)");
+int main(void)
+{
+    int x[15] = {0};
+    int h[15] = {0};
+    int a[15] = {0};
+    int p[15] = {0};
+    int y[15] = {0};
+    int N,L,k,i,j = 0;
+    
+    printf("Enter the length of h(n) :");
+    scanf("%d",&N);
+    printf("Enter the length of x(n) :");
+    scanf("%d",&L);
+    printf("Enter the values of h(n) ");
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&h[i]);
+        
+    }
+    printf("Enter the values of x(n)");
+    for(j=0;j<L;j++)
+    {
+        scanf("%d",&x[j]);
+    }
+    
+    
+    
+    if(N-L!=0)
+    {
+        if(L>N)
+        {
+            for(i=N;i<L;i++)
+                        h[i]=0;
+                        N=L;
+        }
+        for(i=L;i<N;i++)
+                    x[i]=0;
+                    L=N;
+}
+
+
+y[0]=0;
+a[0]=h[0];
+
+for(j=1;j<N;j++)
+            a[j]=h[N-j];
+            
 for(i=0;i<N;i++)
+            y[0]+=x[i]*a[i];
+            
+for(k=1;k<N;k++)
 {
-scanf("%d",&h[i]);
-}
-printf("Enter the values of x(n)");
-for(j=0;j<L;j++)
-{
-scanf("%d",&x[j]);
-}
-
-// padding of zeros to make lengths of h(n) and x(n) equal to K = N+M-1
-
-for(i=N;i<=K;i++)
-{
-h[i]=0;
-}
-for(i=L;i<=K;i++)
-{
-x[i]=0;
-
+    y[k]=0;
+    
+    for(j=1;j<N;j++)
+                p[j]=a[j-1];
+                p[0]=a[N-1];
+    for(i=0;i<N;i++)
+    {
+        a[i]=p[i];
+        y[k]+=x[i]*p[i];
+    }
 }
 
-// linear convolution of h(n) and x(n)
+printf("Circular convolution output is :");
 
-for(i=0;i<K;i++)
+for(i=0;i<=N;i++)
 {
-y[i]=0;
-
-for(j=0;j<=i;j++)
-{
-y[i]=y[i]+((h[j])*(x[i-j]));
+    printf("\n output y[%d] = %d",i,y[i]);
 }
-}
-
-printf("\nLinear convolution output is:");
-
-for(i=0;i<=K;i++)
-
-{
-printf("\n y(%d) = %d",i,y[i]);
-}
-
 while(1);
 }
